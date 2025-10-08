46a71e5e6c3d04fa060404bd133cb41ea17f02b1
kajt <kajt.kajt.kajt.kajt.kajt@gmail.com>
2025-10-08 19:20:38 +0300

    Task 1 complete

---
+++ task01.py
@@ -0,0 +1,24 @@
+from datetime import datetime, date, timedelta
+
+def get_days_from_today(date: str) -> int:
+    """
+    Calculate number of days from today till the given date.
+
+    If given date is in future, negative number is returned.
+    If input string does not match format, None is returned.
+
+    Parameters: 
+        date (str): some date in format "YYYY-MM-DD" (f.e., "2020-10-09")
+    
+    Returns: 
+        int: number of days; 
+    """
+    try:
+        given_date=datetime.strptime(date,"%Y-%m-%d").date()
+    except ValueError:
+        print(f"ERROR: Given input value \"{date}\" does not match format \"YYYY-MM-DD\"!")
+        return None
+    else:
+        curr_date=datetime.today().date()
+        return (curr_date-given_date).days
+
